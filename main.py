ff772013de49f604beb26cb88b893c2d1c6374b7
Muhammad Hazim <hazimutalib96@gmail.com>
2020-11-18 16:16:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import numpy as np
 sns.set()
 from sklearn.model_selection import train_test_split
-from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 import shap
 import base64
@@ -47,17 +47,17 @@ y = df["Interest_Rate"]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
 
-cb=CatBoostRegressor(eval_metric='RMSE')
-cb.fit(x_train, y_train, eval_set=(x_test, y_test),verbose = False)
+xgb=XGBRegressor(max_depth = 4)
+xgb.fit(x_train, y_train)
 
 df1 = pd.DataFrame(index=['R-Squared', 'Root Mean Squared Error'])
-df1['Train Score'] = [cb.score(x_train,y_train), np.sqrt(mean_squared_error(y_train, cb.predict(x_train)))]
-df1['Test Score'] = [cb.score(x_test,y_test), np.sqrt(mean_squared_error(y_test, cb.predict(x_test)))]
+df1['Train Score'] = [xgb.score(x_train,y_train), np.sqrt(mean_squared_error(y_train, xgb.predict(x_train)))]
+df1['Test Score'] = [xgb.score(x_test,y_test), np.sqrt(mean_squared_error(y_test, xgb.predict(x_test)))]
 
 st.dataframe(df1)
 
 
-ex = shap.TreeExplainer(cb)
+ex = shap.TreeExplainer(xgb)
 shap_values = ex.shap_values(x_test)
 shap.initjs()
 fig, ax = plt.subplots()
